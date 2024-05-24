f64997fef1ff6a8f8a85a465a9f9b22b96c4f63d
Ben Field <benjfield@hotmail.com>
2024-05-24 12:02:57 +0100

    fixed with refcell

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,22 +1,23 @@
 
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap};
 
 use regex::Regex;
 advent_of_code::solution!(7);
 
+#[derive(Clone)]
 struct Step {
     name: String,
-    preceding_steps: Vec<Box<Step>>,
-    anteceding_steps: Vec<Box<Step>>,
+    preceding_steps: Vec<RefCell<Step>>,
+    anteceding_steps: Vec<RefCell<Step>>,
     on: bool,
 }
 
 impl Step {
-    fn add_preceding_step(&mut self, other_step: Box<Step>) {
+    fn add_preceding_step(&mut self, other_step: RefCell<Step>) {
         self.preceding_steps.push(other_step)
     }
 
-    fn add_anteceding_step(&mut self, other_step: Box<Step>) {
+    fn add_anteceding_step(&mut self, other_step: RefCell<Step>) {
         self.anteceding_steps.push(other_step)
     }
 }
@@ -27,22 +28,72 @@ pub fn part_one(input: &str) -> Option<u32> {
     .lines()
     .fold(
         HashMap::new(),
-        |mut all_steps: HashMap<&str, Step>, line| {
+        |mut all_steps: HashMap<&str, RefCell<Step>>, line| {
             let (_, [first_step_name, second_step_name]) = re.captures(line).unwrap().extract();
-            let first_step = *all_steps
-                        .entry(first_step_name)
-                        .or_insert(Step{name: first_step_name.to_string(), preceding_steps: Vec::new(), anteceding_steps: Vec::new(), on: false});
 
-            let mut second_step: Step = *all_steps
-            .entry(second_step_name)
-            .or_insert(Step{name: second_step_name.to_string(), preceding_steps: Vec::new(), anteceding_steps: Vec::new(), on: false});
+            let first_step_opt = all_steps.get(first_step_name);
+            let first_step;
+            if first_step_opt.is_none() {
+                first_step = RefCell::new(Step{
+                    name: first_step_name.to_string(),
+                    preceding_steps: Vec::new(),
+                    anteceding_steps: Vec::new(),
+                    on: false,
+                });
+                all_steps.insert(first_step_name, first_step.clone());
+            } else {
+                first_step = first_step_opt.unwrap().to_owned();
+            }
 
-            first_step.add_anteceding_step(Box::new(second_step));
-            second_step.add_preceding_step(Box::new(first_step));
+            let second_step_opt = all_steps.get(second_step_name);
+            let second_step;
+            if second_step_opt.is_none() {
+                second_step = RefCell::new(Step{
+                    name: second_step_name.to_string(),
+                    preceding_steps: Vec::new(),
+                    anteceding_steps: Vec::new(),
+                    on: false,
+                });
+                all_steps.insert(second_step_name, second_step.clone());
+            } else {
+                second_step = second_step_opt.unwrap().to_owned();
+            }
+
+            first_step.borrow_mut().add_anteceding_step(second_step.clone());
+            second_step.borrow_mut().add_preceding_step(first_step.clone());
 
             all_steps
         }
     );
+
+    let mut letters: Vec<String> = Vec::new();
+    let mut next_steps: Vec<RefCell<Step>> = Vec::new();
+
+    for step in steps.values() {
+        if step.borrow().preceding_steps.len() == 0 {
+            next_steps.push(step.clone())
+        }
+    }
+
+    while next_steps.len() > 0 {
+        let mut unsorted_letters:  Vec<String> = Vec::new();
+
+        let mut potential_next_steps: Vec<RefCell<Step>> = Vec::new();
+        for step in next_steps {
+            unsorted_letters.push(step.borrow().name.clone());
+            step.borrow_mut().on = true;
+
+            for anteceding_step in step.borrow().anteceding_steps {
+                potential_next_steps.push(anteceding_step.clone())
+
+            }
+        }
+
+        unsorted_letters.sort();
+        letters.extend(unsorted_letters);
+
+    }
+
 
     None
 }
